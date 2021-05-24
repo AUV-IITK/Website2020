ab341c01d3376064d0a7dedd6a624e4ad27ebf54
Ritam Jana <ritamjana2002@gmail.com>
2021-05-24 13:47:23 +0530

    restructured team page with bootstrap

--- src/views/TeamPage/components/SubsystemHeads/SubsystemHeads.js
+++ src/views/TeamPage/components/SubsystemHeads/SubsystemHeads.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  Container,
-  Row,
-  Col
-} from "reactstrap";
+import React from "react";
+import { Card, Row, Col, Container } from "react-bootstrap"
 import "./SubsystemHeads.css";
 import team from "../../../../data/TeamMembers.json";
 function TalkAbout() {
@@ -19,51 +10,33 @@ function TalkAbout() {
                 <Container>
                     {
                         team.teamData.map((section) => {
-                            console.log(section);
-                            return(
+                            // console.log(section);
+                            return (
                                 <div>
                                     <h1 className="title heading-main">{section.heading}</h1>
                                     <div>
-                                        <Row>
-                                            {section.items.map((teamMember)=>{
-                                                console.log(teamMember);
-                                                return(
-                                                    <Col lg="3 ml-auto mr-auto" sm="6">
-                                                        <Card className="card-profile card-plain card-ssk">
-                                                            <CardBody>
-                                                                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                                                                    <div className="author">
-                                                                        <img
-                                                                            alt="..."
-                                                                            src={require("assets/img/"+ teamMember.image)}
-                                                                            className="image-prof"
-                                                                        />
-                                                                        <></>
-                                                                        <CardTitle tag="h4" className="auv-description-primary pt-3">
-                                                                            {teamMember.name}
-                                                                        </CardTitle>
-                                                                        <h6 className="auv-description-primary"> {teamMember.subheading}</h6>
-                                                                    </div>
-                                                                </a>
-                                                            </CardBody>
-                                                            <CardFooter className="text-center margin-neg">
-                                                                <Button
-                                                                    className="btn-just-icon btn-neutral"
-                                                                    color="link"
-                                                                    href="#pablo"
-                                                                    onClick={(e) => e.preventDefault()}
-                                                                >
-                                                                    <i className="fa fa-facebook flip"/>
-                                                                </Button>
-                                                                <Button
-                                                                    className="btn-just-icon btn-neutral ml-1"
-                                                                    color="link"
-                                                                    href="#pablo"
-                                                                    onClick={(e) => e.preventDefault()}
-                                                                >
-                                                                    <i className="fa fa-linkedin flip"/>
-                                                                </Button>
-                                                            </CardFooter>
+                                        <Row className="rowPadding">
+                                            {section.items.map((teamMember) => {
+                                                // console.log(teamMember);
+                                                return (
+                                                    <Col lg="4 ml-auto mr-auto" sm="6" className="colPadding">
+                                                        <Card>
+                                                            <Card.Body className="cardBody">
+                                                                <img src={require("assets/img/" + teamMember.image)} 
+                                                                className="memberImage" />
+                                                                <Card.Title><h5>{teamMember.name}</h5> </Card.Title>
+                                                                <Card.Text>
+                                                                    <h6>{teamMember.subheading}</h6>
+                                                                </Card.Text>
+                                                                <ul className="socialList">
+                                                                    <li>
+                                                                        <i className="social fa fa-facebook fa-2x" />
+                                                                    </li>
+                                                                    <li>
+                                                                         <i className="social fa fa-linkedin fa-2x" />
+                                                                    </li>
+                                                                </ul>
+                                                            </Card.Body>
                                                         </Card>
                                                     </Col>
                                                 )
