4ec24549886e8f6fb358460f5b84e12894d9478f
Ritam Jana <ritamjana2002@gmail.com>
2021-06-09 00:01:05 +0530

    Styling changes

--- src/views/TeamPage/components/LegacyCarousel/LegacyCarousel.js
+++ src/views/TeamPage/components/LegacyCarousel/LegacyCarousel.js
@@ -3,11 +3,12 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import picture from "assets/img/HD-amazing-photo-underwater.jpg"
 import placeholder from "assets/img/team/author.jpg"
+import "./LegacyCarousel.css"
 
 const LegacyCarousel = () => {
     return (
         <div>
-            <Carousel >
+            {/* <Carousel >
                 <Carousel.Item interval={null} style={{ maxHeight: '500px' }}>
                     <img
                         className="d-block w-100"
@@ -54,7 +55,90 @@ const LegacyCarousel = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            <br />
+            <br /> */}
+            <section id="testimonials" className="testimonials">
+                <div className="container">
+
+                    <div className="section-title" data-aos="zoom-out">
+                        <h2>Testimonials</h2>
+                        <p>What they are saying about us</p>
+                    </div>
+
+                    <div className="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="100">
+                        <div className="swiper-wrapper">
+
+                            <div className="swiper-slide">
+                                <div className="testimonial-item">
+                                    <p>
+                                        <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                  <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                    </p>
+                                    <img src="assets/img/testimonials/testimonials-1.jpg" className="testimonial-img" alt="" />
+                                    <h3>Saul Goodman</h3>
+                                    <h4>Ceo &amp; Founder</h4>
+                                </div>
+                            </div>
+
+                            <div className="swiper-slide">
+                                <div className="testimonial-item">
+                                    <p>
+                                        <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                  <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                    </p>
+                                    <img src="assets/img/testimonials/testimonials-2.jpg" className="testimonial-img" alt="" />
+                                    <h3>Sara Wilsson</h3>
+                                    <h4>Designer</h4>
+                                </div>
+                            </div>
+
+                            <div className="swiper-slide">
+                                <div className="testimonial-item">
+                                    <p>
+                                        <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                  <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                    </p>
+                                    <img src="assets/img/testimonials/testimonials-3.jpg" className="testimonial-img" alt="" />
+                                    <h3>Jena Karlis</h3>
+                                    <h4>Store Owner</h4>
+                                </div>
+                            </div>
+
+                            <div className="swiper-slide">
+                                <div className="testimonial-item">
+                                    <p>
+                                        <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                  <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                    </p>
+                                    <img src="assets/img/testimonials/testimonials-4.jpg" className="testimonial-img" alt="" />
+                                    <h3>Matt Brandon</h3>
+                                    <h4>Freelancer</h4>
+                                </div>
+                            </div>
+
+                            <div className="swiper-slide">
+                                <div className="testimonial-item">
+                                    <p>
+                                        <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                  <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                    </p>
+                                    <img src="assets/img/testimonials/testimonials-5.jpg" className="testimonial-img" alt="" />
+                                    <h3>John Larson</h3>
+                                    <h4>Entrepreneur</h4>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="swiper-pagination"></div>
+                    </div>
+
+                </div>
+            </section>
+
         </div>
     )
 }
