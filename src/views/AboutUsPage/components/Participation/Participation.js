2c0f21f7ad8f6de20c6e3bc2830431f7b3421ea2
Ritam Jana <ritamjana2002@gmail.com>
2021-06-23 22:46:18 +0530

    About Us page modified

--- src/views/AboutUsPage/components/Participation/Participation.js
+++ src/views/AboutUsPage/components/Participation/Participation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Tabs, Tab } from 'react-bootstrap'
+import { Container, Tabs, Tab, Row, Col } from 'react-bootstrap'
 import combinedimage from '../../../../assets/img/competitions.jpg'
 import resimage from '../../../../assets/img/research.jpg'
 import csrimage from '../../../../assets/img/projects.jpg'
@@ -10,79 +10,69 @@ function Posts() {
     return (
         <>
             <div className="section landing-section">
-                <Container >
-                    {/* <Row className="d-flex justify-content-center">
-                        <Col sm="12" lg="6" className="text-center" >
-                            <h2 className="mt-5 mb-3 small-heading">Student Competitions</h2>
-                            <img src={combinedimage} className="w-100 large-image-responsiveness" />
-                            <p className="auv-description-primary">
-                                We aim to participate in National and International
-                                student-level AUV compeititions - Robosub (organised by AUVSI),
-                                Singapore AUV Challenge, and the NIOT-Student AUV Challenge.
-                            </p>
-                        </Col>
-                        <Col sm="12" lg="6" className="text-center">
-                            <h2 className="mt-5  mb-3 small-heading">Research Potential</h2>
-                            <img src={resimage} className="w-100 large-image-responsiveness" />
-                            <p className="auv-description-primary">
-                                Contribute to the development in the fields of marine
-                                technology, and implement cutting-edge research ideas to our
-                                vehicles
-                            </p>
-                        </Col>
-                    </Row >
-                    <Row className="d-flex justify-content-center">
-                        <Col sm="12" lg="6" className="mt-5 text-center">
-                            <h2 className="mt-5 mb-3 small-heading">Training</h2>
-                            <img src={trainimage} className=" w-100 large-image-responsiveness" />
-                            <p className="auv-description-primary">
-                                Team AUV-IITK provides training to all its new recruits in the
-                                various field of Robotics. It is because of this training that
-                                many of the past members of team AUV-IITK are pursuing their
-                                careers in robotics and doing exceptionally well owing to their
-                                strong foundation.
-                            </p>
-                        </Col>
-                        <Col sm="12" lg="6" className=" mt-5 text-center">
-                            <h2 className="mt-5 mb-3 small-heading ">Social Projects</h2>
-                            <img src={csrimage} className="w-100 large-image-responsiveness" />
-                            <p className="auv-description-primary">
-                                Apart from Robotics competitions, team AUV-IITK also actively
-                                participates various hackathon and undertakes projects owing its
-                                responsibility to the society.
-                            </p>
-                        </Col>
-                    </Row> */}
-                    <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-                        <Tab eventKey="home" title="Home" className="tab-body">
-                            <h2 >Training</h2>
-                            <p >
-                                Team AUV-IITK provides training to all its new recruits in the
-                                various field of Robotics. It is because of this training that
-                                many of the past members of team AUV-IITK are pursuing their
-                                careers in robotics and doing exceptionally well owing to their
-                                strong foundation.
-                            </p>
+                <Container className="text-center">
+                    <Tabs defaultActiveKey="studentcompetitions" id="uncontrolled-tab-example" className="">
+                        <Tab eventKey="studentcompetitions" title="Competitions" className="tab-body">
+                            <Row>
+                                <Col md="6" className="my-auto">
+                                    <h2 className="mt-0 mb-3 tab-heading">Student Competitions</h2>
+                                    <p className="tab-content">
+                                        We aim to participate in National and International
+                                        student-level AUV compeititions - Robosub (organised by AUVSI),
+                                        Singapore AUV Challenge, and the NIOT-Student AUV Challenge.
+                                    </p>
+                                </Col>
+                                <Col md="6" className="text-center">
+                                    <img src={combinedimage} className="tab-image" />
+                                </Col>
+                            </Row>
                         </Tab>
-                        <Tab eventKey="profile" title="Profile">
-                            <h2 >Training</h2>
-                            <p >
-                                Team AUV-IITK provides training to all its new recruits in the
-                                various field of Robotics. It is because of this training that
-                                many of the past members of team AUV-IITK are pursuing their
-                                careers in robotics and doing exceptionally well owing to their
-                                strong foundation.
-                            </p>
+                        <Tab eventKey="researchpotential" title="Research" className="tab-body">
+                            <Row>
+                                <Col md="6" className="my-auto">
+                                    <h2 className="mt-0 mb-3 tab-heading">Research Potential</h2>
+                                    <p className="tab-content">
+                                        Contribute to the development in the fields of marine
+                                        technology, and implement cutting-edge research ideas to our
+                                        vehicles
+                                    </p>
+                                </Col>
+                                <Col md="6" className="text-center">
+                                    <img src={resimage} className="tab-image" />
+                                </Col>
+                            </Row>
                         </Tab>
-                        <Tab eventKey="contact" title="Contact" >
-                            <h2 >Training</h2>
-                            <p >
-                                Team AUV-IITK provides training to all its new recruits in the
-                                various field of Robotics. It is because of this training that
-                                many of the past members of team AUV-IITK are pursuing their
-                                careers in robotics and doing exceptionally well owing to their
-                                strong foundation.
-                            </p>
+                        <Tab eventKey="training" title="Training" className="tab-body">
+                            <Row>
+                                <Col md="6" className="my-auto">
+                                    <h2 className="mt-0 mb-3 tab-heading">Training</h2>
+                                    <p className="tab-content">
+                                        Team AUV-IITK provides training to all its new recruits in the
+                                        various field of Robotics. It is because of this training that
+                                        many of the past members of team AUV-IITK are pursuing their
+                                        careers in robotics and doing exceptionally well owing to their
+                                        strong foundation.
+                                    </p>
+                                </Col>
+                                <Col md="6" className="text-center">
+                                    <img src={trainimage} className="mt-3 mb-3 tab-image" />
+                                </Col>
+                            </Row>
+                        </Tab>
+                        <Tab eventKey="socialprojects" title="Community" className="tab-body">
+                            <Row>
+                                <Col md="6" className="my-auto">
+                                    <h2 className="mt-0 mb-3 tab-heading">Social Projects</h2>
+                                    <p className="tab-content">
+                                        Apart from Robotics competitions, team AUV-IITK also actively
+                                        participates various hackathon and undertakes projects owing its
+                                        responsibility to the society.
+                                    </p>
+                                </Col>
+                                <Col md="6" className="text-center">
+                                    <img src={csrimage} className="tab-image" />
+                                </Col>
+                            </Row>
                         </Tab>
                     </Tabs>
                 </Container>
